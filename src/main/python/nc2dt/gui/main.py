f8628d1604f1d917a94daac1cf72032580ce46bc
ekisu <ramonzitosdesorte@hotmail.com>
2019-05-23 21:05:57 -0300

    main: add code to handle nonstandard osu! installs

--- src/main/python/nc2dt/gui/main.py
+++ src/main/python/nc2dt/gui/main.py
@@ -1,5 +1,6 @@
 from fbs_runtime.application_context import ApplicationContext
 from nc2dt.gui.main_window import MainWindow
+from nc2dt.gui.settings import get_osu_path, set_osu_path
 from nc2dt.core.osu_db import OsuDB, get_default_osu_path
 from nc2dt.utils import set_app_context
 from pathlib import Path
@@ -7,12 +8,37 @@ from PyQt5 import QtWidgets
 import sys
 
 if __name__ == "__main__":
-    osu_path = get_default_osu_path()
-    osu_db_file_path = osu_path / Path("osu!.db")
-    osu_db = OsuDB(osu_db_file_path)
-
     appctxt = ApplicationContext()
     set_app_context(appctxt)
+
+    osu_path = get_default_osu_path()
+    settings_osu_path = get_osu_path()
+    if settings_osu_path is not None:
+        osu_path = settings_osu_path
+    osu_db_file_path = osu_path / Path("osu!.db")
+
+    if not osu_db_file_path.exists():
+        QtWidgets.QMessageBox.warning(None, "NC2DT", (
+            "Couldn't find your osu! installation.\n"
+            "Please select the folder where it's installed."
+        ))
+        new_path = QtWidgets.QFileDialog.getExistingDirectory(
+            None,
+            "Please select your osu! installation"
+        )
+        if new_path == "":
+            QtWidgets.QMessageBox.critical(None, "NC2DT", "No folder was selected, exiting.")
+            sys.exit(1)
+        
+        set_osu_path(new_path)
+        osu_db_file_path = Path(new_path) / Path("osu!.db")
+
+    try:
+        osu_db = OsuDB(osu_db_file_path)
+    except FileNotFoundError:
+        QtWidgets.QMessageBox.critical(None, "NC2DT", "Couldn't open osu!.db. Exiting.")
+        sys.exit(1)
+
     main_window = MainWindow(osu_db)
     main_window.show()
     sys.exit(appctxt.app.exec())
---
+++ src/main/python/nc2dt/gui/settings.py
@@ -0,0 +1,17 @@
+from nc2dt.core.osu_db import get_default_osu_path
+from PyQt5.QtCore import QSettings
+from pathlib import Path
+
+def _get_settings():
+    return QSettings("NC2DT", "ekisu")
+
+def get_osu_path():
+    settings = _get_settings()
+    osu_path_str = settings.value("osu_path", type=str)
+    if osu_path_str == "":
+        return None
+    return Path(osu_path_str)
+
+def set_osu_path(path_str):
+    settings = _get_settings()
+    settings.setValue("osu_path", path_str)
