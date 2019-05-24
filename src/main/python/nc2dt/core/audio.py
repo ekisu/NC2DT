8d1fdc5820f49cd7d6733e8145ba8e5668e0927b
ekisu <ramonzitosdesorte@hotmail.com>
2019-05-24 15:04:40 -0300

    audio: check status of the subprocess calls

--- src/main/python/nc2dt/core/audio.py
+++ src/main/python/nc2dt/core/audio.py
@@ -17,32 +17,32 @@ class Audio(object):
         # then speed it back up by 3/2, keeping the pitch.
         temp_decoded_file = NamedTemporaryFile(delete=False)
         temp_decoded_file.close()
-
-        subprocess.run([
-            get_sox_executable(),
-            str(self.path),
-            "-t", "wav",
-            temp_decoded_file.name
-        ])
-        
         temp_soundstretch_output = NamedTemporaryFile(delete=False)
         temp_soundstretch_output.close()
-        subprocess.run([
-            get_soundstretch_executable(),
-            temp_decoded_file.name,
-            temp_soundstretch_output.name,
-            "-rate=-33.3333%",
-            "-tempo=50%"
-        ])
 
-        os.remove(temp_decoded_file.name)
+        try:
+            subprocess.run([
+                get_sox_executable(),
+                str(self.path),
+                "-t", "wav",
+                temp_decoded_file.name
+            ], check=True)
+            
+            subprocess.run([
+                get_soundstretch_executable(),
+                temp_decoded_file.name,
+                temp_soundstretch_output.name,
+                "-rate=-33.3333%",
+                "-tempo=50%"
+            ], check=True)
 
-        subprocess.run([
-            get_sox_executable(),
-            temp_soundstretch_output.name,
-            "-t", "mp3",
-            "-C", "192",
-            str(out_path)
-        ])
-
-        os.remove(temp_soundstretch_output.name)
+            subprocess.run([
+                get_sox_executable(),
+                temp_soundstretch_output.name,
+                "-t", "mp3",
+                "-C", "192",
+                str(out_path)
+            ], check=True)
+        finally:
+            os.remove(temp_decoded_file.name)
+            os.remove(temp_soundstretch_output.name)
